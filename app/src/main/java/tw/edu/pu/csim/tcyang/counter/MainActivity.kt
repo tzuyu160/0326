328f936ff031e1af9c89a82772e07c16e64c83a1
tcyang <tcyang@gm.pu.edu.tw>
2021-03-16 09:48:20 +0800

    OnClickListener

--- app/src/main/java/tw/edu/pu/csim/tcyang/counter/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/tcyang/counter/MainActivity.kt
@@ -2,11 +2,21 @@ package tw.edu.pu.csim.tcyang.counter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    var counter: Int = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        txv.setOnClickListener(this)
+    }
+
+    override fun onClick(p0: View?) {
+        counter++
+        txv.text = counter.toString()
     }
 }
