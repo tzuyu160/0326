8be0a0ebcafe2545f03dc4762f3c7f1ca8472733
tcyang <tcyang@gm.pu.edu.tw>
2021-03-16 09:54:10 +0800

    Button歸零

--- app/src/main/java/tw/edu/pu/csim/tcyang/counter/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/tcyang/counter/MainActivity.kt
@@ -13,10 +13,16 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         txv.setOnClickListener(this)
+        btn.setOnClickListener(this)
     }
 
-    override fun onClick(p0: View?) {
-        counter++
+    override fun onClick(view: View) {
+        if (view.id.equals(R.id.txv)){
+            counter++
+        }
+        else{
+            counter = 0
+        }
         txv.text = counter.toString()
     }
 }
